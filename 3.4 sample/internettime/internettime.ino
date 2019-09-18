c4288ec1658d61df71235333bac04410d5088044
David Lee <54704426+GoGoCom@users.noreply.github.com>
2019-09-18 22:42:23 +1000

    Create internettime.ino

---
+++ 3.4 sample/internettime/internettime.ino
@@ -0,0 +1,132 @@
+
+#include <TimeLib.h>       
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
+#define TimeGab  9
+
+#ifndef STASSID
+#define STASSID "COREANHOUSE"
+#define STAPSK  "COREAN000"
+#endif
+
+const char* ssid     = STASSID;
+const char* password = STAPSK;
+
+
+
+void setup() {
+  Serial.begin(9600);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Get time from Internaet  
+  setTimeFromUTCToLocal( TimeGab );
+}
+
+void loop() {
+  time_t t;
+  String strDateTime; 
+   
+  t = now();
+
+  strDateTime = String(hour(t)) + ":" + String(minute(t)) + ":" + String(second(t)) + " " + String(dayShortStr(weekday(t))) + " " + String(day(t)) + "/" + String(monthShortStr(month(t))) + "/" + String(year(t)) + " UTC" + String(TimeGab) + "h" ;
+  Serial.println(strDateTime);   
+  
+  delay(1000);    
+}
+
+void setTimeFromUTCToLocal(time_t tgab )
+{
+  String  tmp_s, strDateTime;
+  int     i;
+  int     c_year, c_month, c_day, c_hour, c_minute, c_sec;  
+  time_t  ltc;
+     
+    strDateTime = getTimeFromInternet( );
+
+    tmp_s = strDateTime.substring(8 , 11);
+    for(i=1; i<13 ; i++)
+    {
+      if ( String(monthShortStr(i)).equals(tmp_s) ) c_month = i;
+    }
+   
+    c_day = strDateTime.substring(5 , 7).toInt();
+    c_year = strDateTime.substring(12,16).toInt();
+    c_hour = strDateTime.substring(17,19).toInt();
+    c_minute = strDateTime.substring(20,22).toInt();        
+    c_sec = strDateTime.substring(23,25).toInt();    
+
+    setTime(c_hour, c_minute, c_sec, c_day, c_month, c_year); // Set Time to UTC
+
+    ltc    = now() + tgab * 3600;
+    
+    setTime(ltc); // Set Local Time
+    tmp_s  = String(tgab) + "h";
+    printTime(ltc, const_cast<char*> ( tmp_s.c_str()) );     
+}
+
+String getTimeFromInternet(void )
+{
+ String tmp_s;
+  
+  WiFiClient client;
+  while (!!!client.connect("google.com", 80)) {
+    Serial.println("MSG\tconnection failed, retrying...");
+    delay(2000);
+  }
+
+  client.print("HEAD / HTTP/1.1\r\n\r\n");
+ 
+  while(!!!client.available()) {
+     yield();
+  }
+
+  while(client.available()){
+    if (client.read() == '\n') {    
+      if (client.read() == 'D') {    
+        if (client.read() == 'a') {    
+          if (client.read() == 't') {    
+            if (client.read() == 'e') {    
+              if (client.read() == ':') {    
+                client.read();
+                tmp_s = client.readStringUntil('\r');
+                client.stop();
+                return tmp_s;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+void printTime(time_t t, char *tf)
+{
+    char  buf[200];
+   String week_s, month_s;
+
+    week_s  = String(dayShortStr(weekday(t)));
+    month_s = String(monthShortStr(month(t)));
+
+    sprintf(buf, "MSG\t%02d:%02d:%02d %s %2d-%s-%4d UTC%s", hour(t), minute(t), second(t), week_s.c_str(), day(t), month_s.c_str(), year(t), tf );
+    Serial.println(buf);    
+    delay(2000);
+}
