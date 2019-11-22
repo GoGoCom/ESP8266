fa576ea31fd564655a4ddc9b4a30fe1853e7c3d6
David Lee <54704426+GoGoCom@users.noreply.github.com>
2019-11-23 05:49:06 +1100

    Create PointerInitialize.ino

---
+++ 1.4 sample/PointerInitialize/PointerInitialize.ino
@@ -0,0 +1,21 @@
+
+
+int  MathScore[10] = { 80, 90, 10 }; 
+char *StudentName = "gildong";
+char *StudentNames[10] = { "gildong","cheolsu","soonhee","yong" };
+
+
+void setup() {
+  // put your setup code here, to run once:
+int *MathScorePtr;
+
+
+MathScorePtr = &MathScore[5]; 
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
