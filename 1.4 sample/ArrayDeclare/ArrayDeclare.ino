c85dac57e1e245237069a465a5f60f15bf3fe0c4
David Lee <54704426+GoGoCom@users.noreply.github.com>
2019-11-23 05:48:59 +1100

    Create ArrayDeclare.ino

---
+++ 1.4 sample/ArrayDeclare/ArrayDeclare.ino
@@ -0,0 +1,17 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+ int  MathScore[10]; 
+
+  
+  MathScore[0] = 80;
+  MathScore[1] = 90;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
