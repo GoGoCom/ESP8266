4c313d3d73dcdec0a6e82c07394f4551cafcf48b
David Lee <54704426+GoGoCom@users.noreply.github.com>
2019-11-23 05:49:03 +1100

    Create ArrayInitialize.ino

---
+++ 1.4 sample/ArrayInitialize/ArrayInitialize.ino
@@ -0,0 +1,15 @@
+
+
+int  MathScore[10] = { 80, 90, 10 }; 
+char StudentName[10] = { 'g','i','l','d','o','n','g' };
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
