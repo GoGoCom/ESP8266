d65673453485e3530912da6f136d9c47851fa0a7
David Lee <54704426+GoGoCom@users.noreply.github.com>
2019-11-23 05:48:54 +1100

    Create VariableInitialize.ino

---
+++ 1.3 sample/VariableInitialize/VariableInitialize.ino
@@ -0,0 +1,18 @@
+
+int EnglishScore = 90, MathScore = 80 ;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  int TotalScore;
+
+
+  TotalScore =  EnglishScore + MathScore ;   
+ 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
