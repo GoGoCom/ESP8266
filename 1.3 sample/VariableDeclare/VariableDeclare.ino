0ffc8e05be473f003f651a204b6452d11bd7ed19
David Lee <54704426+GoGoCom@users.noreply.github.com>
2019-11-23 05:48:50 +1100

    Create VariableDeclare.ino

---
+++ 1.3 sample/VariableDeclare/VariableDeclare.ino
@@ -0,0 +1,18 @@
+
+void setup() {
+  // put your setup code here, to run once:
+
+  int TotalScore , EnglishScore, MathScore ;
+
+ EnglishScore = 90;
+ MathScore = 80;
+ 
+ TotalScore =  EnglishScore + MathScore ;   
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
