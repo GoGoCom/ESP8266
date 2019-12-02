7ada3a6a496c30594cb86628bc3fe211b512eff2
David Lee <54704426+GoGoCom@users.noreply.github.com>
2019-12-02 11:30:13 +1100

    Create ClassDogRobot.cpp

---
+++ 1.8 sample/libraries/ClassRobots/ClassDogRobot.cpp
@@ -0,0 +1,16 @@
+
+#include "ClassDogRobot.h"
+
+void dogROBOT::Thinking( ) {
+   Serial.println("Dog is thinking.");  
+}
+
+void dogROBOT::Running( ) {
+   Serial.println("Dog is running.");  
+}
+
+void dogROBOT::Walking( ) {
+   Serial.println("Dog is walking.");  
+}
+
+
