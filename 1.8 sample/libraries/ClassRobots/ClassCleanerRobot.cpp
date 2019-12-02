7ffa735c21941cbd51435cc4ceac9806d23ae269
David Lee <54704426+GoGoCom@users.noreply.github.com>
2019-12-02 11:30:21 +1100

    Create ClassCleanerRobot.cpp

---
+++ 1.8 sample/libraries/ClassRobots/ClassCleanerRobot.cpp
@@ -0,0 +1,16 @@
+
+#include "ClassCleanerRobot.h"
+
+void cleanerROBOT::Thinking( ) {
+   Serial.println("Cleaner is thinking.");  
+}
+
+void cleanerROBOT::Running( ) {
+   Serial.println("Cleaner is running.");  
+}
+
+void cleanerROBOT::Walking( ) {
+   Serial.println("Cleaner is walking.");  
+}
+
+
